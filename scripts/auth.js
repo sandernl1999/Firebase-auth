2a2524263c84c51d30b3d336df3a2a75ea1e14cd
Sander Nordby Larsen <sanderlarsen@Sanders-MacBook-Pro-2.local>
2021-03-05 11:12:27 +0100

    first commit

---
+++ scripts/auth.js
@@ -0,0 +1,82 @@
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        db.collection('guides').onSnapshot(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        }, err => {
+            console.log(err.message)
+        });
+        
+    }  else {
+        setupUI();
+        setupGuides([]);
+    }
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+    title: createForm['title'].value,
+    content: createForm['content'].value
+    }).then(() => {
+        // close the modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    }).catch(err => {
+        console.log(err.message)
+    })
+})
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value
+        });   
+    }).then(() => {
+       const modal = document.querySelector('#modal-signup');
+       M.Modal.getInstance(modal).close();
+       signupForm.reset(); 
+    });
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+e.preventDefault();
+auth.signOut().then(() => {
+  console.log('user signed out');   
+ });
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        // close the login modal and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
